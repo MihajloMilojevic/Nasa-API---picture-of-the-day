5af12da5cabe925cbcb728820591a2f2b0f5d0fc
MihajloESTS <milojevicm374@gmail.com>
2021-12-29 00:01:11 +0100

    Version 2 start - modal showing, functions missing

--- script.js
+++ script.js
@@ -1,112 +1,10 @@
+import Modal from "./modal.js";
 
 const API_KEY = "A4UdlhlqMgspVyJRdjPjirtXGJl3XTv1PzOayBZ0";
 
-const dan = document.getElementById("dan");
-const mesec = document.getElementById("mesec");
-const godina = document.getElementById("godina");
+document.getElementById("img").onresize = setImageDimensions;
 
-const meseci = [
-    "januar", "februar", "mart", "april",
-    "maj", "jun", "jul", "avgust",
-    "septembar", "oktobar", "novembar", "decembar"
-];
-const dani = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; 
-
-document.addEventListener("DOMContentLoaded", () => {
-    PromenaSlika(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&&thumbs=true`);
-        
-    const danas = new Date();
-    if (PrestupnaGodina(danas.getFullYear()))
-        dani[1] = 29;
-    else
-        dani[1] = 28;
-    for (let i = 0; i < 12; i++) {
-        const option = document.createElement("option");
-        option.value = i + 1;
-        option.innerText = meseci[i];
-        mesec.appendChild(option);
-    }
-    for (let i = 1995; i <= danas.getFullYear(); i++) {
-        const option = document.createElement("option");
-        option.value = i;
-        option.innerText = i;
-        godina.appendChild(option);
-    }
-    for (let i = 1; i <= dani[danas.getMonth()]; i++) {
-        const option = document.createElement("option");
-        option.value = i;
-        option.innerText = i;
-        dan.appendChild(option);
-    }
-    mesec.value = danas.getMonth() + 1;
-    godina.value = danas.getFullYear();
-    dan.value = danas.getDate();
-})
-
-function GodinaPromena() {
-    if (PrestupnaGodina(godina.value))
-        dani[1] = 29;
-    else
-        dani[1] = 28;
-    MesecPromena();
-}
-
-function MesecPromena() {
-    dan.innerHTML = "";
-    for (let i = 1; i <= dani[mesec.value - 1]; i++) {
-        const option = document.createElement("option");
-        option.value = i;
-        option.innerText = i;
-        dan.appendChild(option);
-    }
-    dan.value = 1;
-}
-
-function Danas()
-{
-    
-    const danas = new Date();
-    dan.innerHTML = "";
-    mesec.innerHTML = "";
-    godina.innerHTML = "";
-    if (PrestupnaGodina(danas.getFullYear()))
-        dani[1] = 29;
-    else
-        dani[1] = 28;
-    for (let i = 0; i < 12; i++) {
-        const option = document.createElement("option");
-        option.value = i + 1;
-        option.innerText = meseci[i];
-        mesec.appendChild(option);
-    }
-    for (let i = 1995; i <= danas.getFullYear(); i++) {
-        const option = document.createElement("option");
-        option.value = i;
-        option.innerText = i;
-        godina.appendChild(option);
-    }
-    for (let i = 1; i <= dani[danas.getMonth()]; i++) {
-        const option = document.createElement("option");
-        option.value = i;
-        option.innerText = i;
-        dan.appendChild(option);
-    }
-    mesec.value = danas.getMonth() + 1;
-    godina.value = danas.getFullYear();
-    dan.value = danas.getDate();
-}
-
-function SlikaPromena() {
-    var q = `${godina.value}-${mesec.value}-${dan.value}`;
-    const URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${q}&thumbs=true`;
-    PromenaSlika(URL);
-}
-
-function PrestupnaGodina(god) {
-    return ((0 == god % 4) && (0 != god % 100) || (0 == god % 400))
-}
-
-function setDimensions()
+function setImageDimensions() // seting dimensions of an the image so that the ratio of width and height stay constant
 {   
     const slika = document.getElementById("slika");
     const img = new Image();
@@ -125,19 +23,17 @@ function setDimensions()
     }
 }
 
-async function PromenaSlika(URL) {
+async function APICall(URL) { // makes an API request and changes the image
     try
     {
         const res = await fetch(URL);
         if(res.ok)
         {
             const data = await res.json();
-            const slika = document.getElementById("slika")
+            const image = document.getElementById("img")
             const src = data.media_type === "video" ? data.thumbnail_url : data.hdurl || data.url;
             //console.log(data);
-            slika.src = src;
-            const img = new Image();
-            img.src = src;
+            image.src = src;
         }
         else
             throw new Error("Greska pri uzimanju podataka");
@@ -145,9 +41,17 @@ async function PromenaSlika(URL) {
     }
     catch(error)
     {
-        document.getElementById("slika").src = "./nasa.png";
+        document.getElementById("img").src = "./nasa.png";
         console.error(error);
     }
 }
+const m = new Modal();
 
-document.getElementById("slika").onresize = setDimensions;
+document.addEventListener("DOMContentLoaded", () => {
+    APICall(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&&thumbs=true`);
+    m.Add();
+})
+
+document.addEventListener("keydown", (e) => {
+    m.Show();
+})
